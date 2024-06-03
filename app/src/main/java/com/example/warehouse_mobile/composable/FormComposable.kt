aed269e59fbf39d13fbc3f5181d0f037cc5bd9b2
IvoRum <ivailo.rumenov@outlook.com>
2024-06-03 21:08:52 +0300

    Form fix

--- app/src/main/java/com/example/warehouse_mobile/composable/FormComposable.kt
+++ app/src/main/java/com/example/warehouse_mobile/composable/FormComposable.kt
@@ -1,7 +1,10 @@
 package com.example.warehouse_mobile.composable
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
@@ -13,6 +16,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SearchShipmentForm() {
@@ -21,32 +26,35 @@ fun SearchShipmentForm() {
     var stock by remember { mutableStateOf("") }
     var telephone by remember { mutableStateOf("") }
 
-    Column {
-        Row {
-            OutlinedTextField(
-                value = address,
-                onValueChange = { address = it },
-                label = { Text("Address") }
-            )
-            OutlinedTextField(
-                value = client,
-                onValueChange = { client = it },
-                label = { Text("Client") }
-            )
-        }
-        Row {
-            OutlinedTextField(
-                value = stock,
-                onValueChange = { stock = it },
-                label = { Text("Stock") }
-            )
-            OutlinedTextField(
-                value = telephone,
-                onValueChange = { telephone = it },
-                label = { Text("Thephone") }
-            )
-        }
-        Button(onClick = { /*TODO*/ }){
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = address,
+            onValueChange = { address = it },
+            label = { Text("Address") }
+        )
+        OutlinedTextField(
+            value = client,
+            onValueChange = { client = it },
+            label = { Text("Client") }
+        )
+        OutlinedTextField(
+            value = stock,
+            onValueChange = { stock = it },
+            label = { Text("Stock") }
+        )
+        OutlinedTextField(
+            value = telephone,
+            onValueChange = { telephone = it },
+            label = { Text("Thephone") }
+        )
+
+        Button(onClick = { /*TODO*/ }) {
             Icon(Icons.Rounded.Search, contentDescription = "Search icon")
         }
     }
